0a1cebaba0ec11f7a1cc507164e420147a77f829
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-01-05 02:30:52 +0000

    Add files via upload

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -7,9 +7,12 @@ package database_builder;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 
@@ -31,7 +34,14 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        final ObservableList<CheckMenuItem> listFilter = FXCollections.observableArrayList();
+        final  MenuButton menuButton = new MenuButton("Modalities");
+        CheckMenuItem item1 = new CheckMenuItem("T1");
+        CheckMenuItem item2 = new CheckMenuItem("T1C");
+        CheckMenuItem item3 = new CheckMenuItem("T2");
+        listFilter.addAll(item1, item2, item3);
+        menuButton.getItems().addAll(listFilter);
+        menuButton.setId("menuButton");
     }    
     
 }
